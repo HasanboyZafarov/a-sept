8e48c48add51c879d80c06b6a404d940fb6ccc4d
Hasanboy Zafarov <103839294+HasanboyZafarov@users.noreply.github.com>
2025-08-16 10:10:01 +0500

    Add initial assets and base project files
    Added SVG logo, base JavaScript files, main HTML, and initial SASS/CSS files for global styles, layout, and utilities. This sets up the foundational structure and assets for the project.

---
+++ assets/js/base.js
@@ -0,0 +1,37 @@
+let bars = document.querySelector('.header__content__right__bars')
+let list = document.querySelector('.header__content__list')
+let header = document.querySelector('.header');
+
+bars.addEventListener('click', () => {
+    if (bars.classList.contains('active')) {
+        bars.classList.remove('active');
+    } else {
+        bars.classList.add('active');
+    }
+    list.classList.toggle('active');
+})
+
+window.addEventListener('click', (el) => {
+    if (!bars.contains(el.target) && !list.contains(el.target)) {
+        list.classList.remove('active')
+        bars.classList.remove('active')
+    }
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY) {
+        list.classList.remove('active')
+        bars.classList.remove('active')
+    } if (window.scrollY > 50) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 991) {
+        bars.classList.remove('active')
+        list.classList.remove('active')
+    }
+})
